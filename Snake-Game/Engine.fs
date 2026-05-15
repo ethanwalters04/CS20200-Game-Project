970ec27499ba07518827edfa4fc79ab52eb7507d
Ethan Walters <walterse6@cardiff.ac.uk>
2026-05-15 23:27:07 +0900

    Out of bounds func

--- Snake-Game/Engine.fs
+++ Snake-Game/Engine.fs
@@ -21,3 +21,8 @@ let safePosition (occupied: Position list) =
     randomPositions
     |> Seq.filter (fun pos -> not (List.contains pos occupied)) // Filters out occupied positions
     |> Seq.head // Gets the first position of the filtered sequence
+
+// Boolean function - check if position is out of bounds
+let isOutOfBounds (pos: Position) =
+    pos.X <= 0 || pos.X >= boardWidth - 1 || pos.Y <= 0 || pos.Y >= boardHeight - 1
+    
