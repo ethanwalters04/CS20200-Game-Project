0a8bc93550a279939e84b43de73ded5b6ff339e5
Ethan Walters <walterse6@cardiff.ac.uk>
2026-05-15 23:46:29 +0900

    Initial game state set up

--- Snake-Game/Engine.fs
+++ Snake-Game/Engine.fs
@@ -22,7 +22,25 @@ let safePosition (occupied: Position list) =
     |> Seq.filter (fun pos -> not (List.contains pos occupied)) // Filters out occupied positions
     |> Seq.head // Gets the first position of the filtered sequence
 
-// Boolean function - check if position is out of bounds
+// Check if a position is out of bounds
 let isOutOfBounds (pos: Position) =
     pos.X <= 0 || pos.X >= boardWidth - 1 || pos.Y <= 0 || pos.Y >= boardHeight - 1
-    
+
+// Set up the initial game state
+let private initGame (startDelay: int, speedStep: int) =
+    let initSnake = {
+        Head = { X = boardWidth / 2; Y = boardHeight / 2 } // Head in centre of board
+        Body = [
+            { X = boardWidth / 2; Y = (boardHeight / 2) + 1 }
+            { X = boardWidth / 2; Y = (boardHeight / 2) + 2 }
+        ]
+        CurrentDir = Up
+    }
+
+    let initGoodFoodPos = safePosition (initSnake.Head :: initSnake.Body)
+    let initGoodFood = Normal initGoodFoodPos
+
+    let initBadFoodPos = safePosition (initGoodFoodPos :: initSnake.Head :: initSnake.Body)
+    let initBadFood = { Pos = initBadFoodPos }
+
+    Playing (initSnake, initGoodFood, [initBadFood], 0, startDelay, speedStep)
